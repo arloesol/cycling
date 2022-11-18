fe121147fe430c6ca89db27c0dd839a4fe31f698
arloesol <john@arloesol.com>
2022-11-18 21:01:08 +0100

    close dev feat-54-tooltips - closes #54

--- bin/git_main_to_preprod.sh
+++ bin/git_main_to_preprod.sh
@@ -8,9 +8,10 @@ if [ "$prlist" != "[]" ]; then
 fi
 stash=$(git stash) || { printf "can't git stash"; exit 6; }
 git fetch || { printf "can't git fetch"; exit 1; }
+git checkout main || { printf "can't checkout main"; exit 7; }
 git pull origin main --ff-only || { printf "can't git pull main"; exit 2; }
 git branch -f preprod main || { printf "can't align preprod with main"; exit 3; }
 git push -f origin preprod || { printf "can't force push preprod to main repo"; exit 4; }
 if [ "$stash" != "No local changes to save" ]; then 
-  git stash pop || { printf "can't put stash back"; exit 7; }
+  echo "do a 'git stash pop' to get source back"
 fi
