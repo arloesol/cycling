f6690371b084d98415e4cb44a4438a2594a6d608
arloesol <john@arloesol.com>
2022-11-18 19:55:51 +0100

    close dev cont-9-Blog_content - closes #9

--- bin/git_main_to_preprod.sh
+++ bin/git_main_to_preprod.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 # NO checks !!! - should really check if local and remote are synched first !
-git fetch || ( printf "can't git fetch"; exit 1 )
-git pull origin main --ff-only || ( printf "can't git pull main"; exit 2 )
-git branch -f preprod main || ( printf "can't align preprod with main"; exit 3 )
-git push -f origin preprod || ( printf "can't force push preprod to main repo"; exit 4 )
+prlist=$(gh pr list --json number,body)
+if [ "$prlist" != "[]" ]; then
+  printf "ERROR - there are open pull requests - close them first"
+  gh pr list
+  exit 5
+fi
+stash=$(git stash) || { printf "can't git stash"; exit 6; }
+git fetch || { printf "can't git fetch"; exit 1; }
+git pull origin main --ff-only || { printf "can't git pull main"; exit 2; }
+git branch -f preprod main || { printf "can't align preprod with main"; exit 3; }
+git push -f origin preprod || { printf "can't force push preprod to main repo"; exit 4; }
+if [ "$stash" != "No local changes to save" ]; then 
+  git stash pop || { printf "can't put stash back"; exit 7; }
+fi
